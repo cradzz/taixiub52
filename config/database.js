7dd8856c6b0d7605f9fe1dfe591e703c97144daa
cradzz <120325392+cradzz@users.noreply.github.com>
2022-12-11 22:59:51 +0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb://localhost:27017',
+	'options': {
+		//'user':   'root',
+		//'pass':   '',
+		'dbName': 'X29', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
